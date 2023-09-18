6ed8620a773bcb56d8f0175ee3ddca63f94b06b5
Volodia <hryhorskyiv@gmail.com>
2023-09-18 20:29:51 +0300

    add links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'post/index'
   devise_for :users
+  root to: 'post#index'
+  
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
