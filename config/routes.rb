2a1e5d49ab6dede1313ec0dd3fcb2c50a823633a
Volodia <hryhorskyiv@gmail.com>
2023-09-20 22:06:11 +0300

    add actions post and rename controller posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root to: 'post#index'
+  root to: 'posts#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
