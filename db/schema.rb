81817b9967e4b69d05341424960b4c649f76091f
Volodia <hryhorskyiv@gmail.com>
2023-09-22 21:16:24 +0300

    destroy migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_21_114512) do
-  create_table "post", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
