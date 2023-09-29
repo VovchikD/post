00a0d02edc8e6a579fa85d35b025dc58841f28b2
Volodia <hryhorskyiv@gmail.com>
2023-09-29 16:38:55 +0300

    add test comment create and destroy

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,5 +13,5 @@ RSpec.configure do |config|
   config.fixture_path = Rails.root.join('spec/fixtures').to_s
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-        .config.filter_rails_from_backtrace!
+  config.filter_rails_from_backtrace!
 end
