c3351f49bb2f45d462458e1f67c315fce564b4f5
Volodia <hryhorskyiv@gmail.com>
2023-10-05 13:17:20 +0300

    refactoring test comments

--- spec/controller/comments_controller_spec.rb
+++ spec/controller/comments_controller_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments_Controller', type: :controller do
+  before do
+    @controller = CommentsController.new
+  end
+  
   it 'create comment' do
+    post = Post.create(title: 'Title post', body: 'Body post')
     expect do
-      post :create, params: { comment: 'New Comment', post_id: post.id }
-    end
+      patch :create, params: { post_id: post.id, comment: { content: 'A comment' } }
+    end.to change(Comment, :count).by(1)
   end
   it 'destroy comment' do
     expect do
