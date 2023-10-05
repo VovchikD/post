aeea6cf09b8aff0631e9b97c26f207c497e3816d
Volodia <hryhorskyiv@gmail.com>
2023-10-05 20:36:52 +0300

    add comments#create test

--- spec/controller/comments_controller_spec.rb
+++ spec/controller/comments_controller_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 
 RSpec.describe 'CommentsController', type: :controller do 
+  let(:post) { FactoryBot.create(:post) }
+
   it 'creates a comment' do
     expect do
-      post :create, params: { post_id: post.id, comment: { content: 'A comment' } }
+      post :create, params: {  comment: { content: 'A comment' }, post_id: post.id   }
     end.to change(Comment, :count).by(1)
   end
   it 'destroy comment' do
