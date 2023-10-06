9d198a4c8ab252d1ae690f3d7b97a787ac213691
Volodia <hryhorskyiv@gmail.com>
2023-10-06 20:31:46 +0300

    add new comment#test

--- spec/controller/comments_controller_spec.rb
+++ spec/controller/comments_controller_spec.rb
@@ -2,16 +2,21 @@ require 'rails_helper'
 
 
 RSpec.describe 'CommentsController', type: :controller do 
-  let(:post) { FactoryBot.create(:post) }
+  let(:post)    { FactoryBot.create(:post) }
+  let(:comment) { FactoryBot.create(:comment) }
 
   it 'creates a comment' do
     expect do
-      post :create, params: {  comment: { content: 'A comment' }, post_id: post.id   }
-    end.to change(Comment, :count).by(1)
+      post :create, params: { comment: { content: 'A comment' }, post_id: post.id }
+      created_comment = Comment.find(content: 'A comment')
+      created_comment.to be_present
   end
+end
   it 'destroy comment' do
     expect do
       delete :destroy, params: { id: comment.id }
+      destroy_comment = Comment.find_by(id: comment.id)
+      destroyed_comment.to be_nil
       end
     end
 end
