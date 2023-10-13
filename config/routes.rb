e496ab749b4f81c746814612029690b361270b7e
Volodia <hryhorskyiv@gmail.com>
2023-10-13 13:38:06 +0300

    add view form comments to comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: %i[create]
   end
+  resources :comments do
+    resources :comments
+  end
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
