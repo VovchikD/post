a36ec3f2a777dd15192fc2f7bd3eef4af3ab37f2
Volodia <hryhorskyiv@gmail.com>
2023-10-15 20:26:32 +0300

    add form to reply

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,14 @@
 
 Rails.application.routes.draw do
   resources :posts do
-    resources :comments, only: %i[create]
-  end
-  resources :comments do
-    resources :comments
+    resources :comments, only: %i[create destroy]
   end
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   root to: 'posts#index'
-  resources :comments, only: %i[destroy]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
