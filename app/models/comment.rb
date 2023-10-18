19cc4e72aa161b40df272329458017beaae52430
Volodia <hryhorskyiv@gmail.com>
2023-10-18 18:56:43 +0300

    add partial form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :parent,   class_name: 'Comment',   optional: true
-  has_many   :comments, foreign_key: :parent_id, dependent: :destroy
+  has_many   :comments, class_name: 'Comment',   foreign_key: :parent_id, dependent: :destroy
 end
