f082c3e6fbdadeee589f43f4a39dbda5eefb80a9
Volodia <hryhorskyiv@gmail.com>
2023-10-19 19:24:27 +0300

    run rubocop and rspec

--- db/migrate/20231012164926_add_parent_to_comments.rb
+++ db/migrate/20231012164926_add_parent_to_comments.rb
@@ -2,6 +2,6 @@
 
 class AddParentToComments < ActiveRecord::Migration[7.0]
   def change
-    add_column :comments, :parent_id, :integer, :null => true
+    add_column :comments, :parent_id, :integer, null: true
   end
 end
