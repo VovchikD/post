fc4288c418d2b013e199b1028c4933cbc9e617ca
Volodia <hryhorskyiv@gmail.com>
2023-11-05 20:34:29 +0200

    fix code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :find_post, only: [:create]
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params.merge(user: current_user))
     if @comment.save
       author = @comment.post.user
-      unless (author == @comment.user)
-        CommentMailer.new_comment(author, @comment).deliver_later
-      end
-      redirect_to post_path(params[:post_id])
+      CommentMailer.new_comment(author, @comment).deliver_later unless author == @comment.user
+      redirect_to post_path(@post)
     else
       render 'posts/show'
     end
@@ -22,6 +21,10 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def find_post
+    @post ||= Post.find(params[:post_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:content, :parent_id)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :find_post, only: %i[show destroy]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
@@ -23,12 +23,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to root_url
   end
 
   private
+
+  def find_post
+    @post ||= Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :body)
