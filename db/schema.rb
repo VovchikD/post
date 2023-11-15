c37f4ced2282903252ed66c89d05c778d562a96c
Volodia <hryhorskyiv@gmail.com>
2023-11-15 12:12:26 +0200

    add change to database config

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_04_152537) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "parent_id"
