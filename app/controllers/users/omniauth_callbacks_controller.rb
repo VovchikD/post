9cdb696181d15d085ce8ab9501c4b1adcb32a82a
Volodia <hryhorskyiv@gmail.com>
2023-12-21 12:31:21 +0200

    change scope users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-module Users
-  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+module Authentications
+  class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
-        success_google_oauth2
+        success_google_auth
       else
-        failed_google_oauth2
+        failed_google_auth
       end
     end
 
@@ -16,30 +16,30 @@ module Users
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
-        success_facebook_oauth2
+        success_facebook_auth
       else
-        failed_facebook_oauth2
+        failed_facebook_auth
       end
     end
 
     private
 
-    def success_facebook_oauth2
+    def success_facebook_auth
       sign_in_and_redirect @user, event: :authentication
       flash[:notice] = 'Success'
     end
 
-    def failed_facebook_oauth2
+    def failed_facebook_auth
       session['devise.facebook_data'] = request.env['omniauth.auth'].except('extra')
       redirect_to new_user_registration_path, alert: @user.errors.full_messages
     end
 
-    def success_google_oauth2
+    def success_google_auth
       sign_in_and_redirect @user, event: :authentication
       flash[:notice] = 'Success'
     end
 
-    def failed_google_oauth2
+    def failed_google_auth
       session['devise.google_data'] = request.env['omniauth.auth'].except('extra')
       redirect_to new_user_registration_path, alert: @user.errors.full_messages
     end
