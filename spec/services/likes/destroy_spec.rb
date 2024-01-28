d3d8cd74669c9bcac6814e99ab8500a1b7d23eb1
Volodia <hryhorskyiv@gmail.com>
2024-01-28 12:19:38 +0200

    add test for services

---
+++ spec/services/likes/destroy_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Likes::Destroy, type: :service do
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user: user) }
+  let(:like) { create(:like, user: user, target: post) }
+
+  it 'when like destroy success' do
+    result = Likes::Destroy.call(user: user, like_id: like.id)
+    expect(result[:status]).to eq(:success)
+    expect(result[:record]).to eq(like)
+  end
+
+  it 'when like destroy failure' do
+    allow_any_instance_of(Like).to receive(:destroy)
+    result = Likes::Destroy.call(user: user, like_id: like.id)
+    expect(result[:status]).to eq(:failure)
+    expect(result[:record]).to eq(like)
+  end
+end
