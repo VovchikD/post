1a3b3862626856c9ee9f581e5a574b0018f3a23f
Volodia <hryhorskyiv@gmail.com>
2024-02-18 18:34:49 +0200

    changed sessions api

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,29 +10,37 @@ module Api
     def respond_with(resource, _opts = {})
       render json: {
         status: { code: 200, message: 'Logged in successfully.' },
-        data: UserSerializer.render(resource)
+        data: UserSerializer.render(resource),
+        token: generate_jwt_token(resource)
       }, status: :ok
     end
 
     def respond_to_on_destroy
       jwt_payload = decode_jwt_payload
       if jwt_payload.present?
-        current_user = find_user_by_jwt_payload(jwt_payload)
-        current_user ? success_sign_out : fail_sign_out
+        find_user_by_jwt_payload(jwt_payload)
+        success_sign_out
       else
-        fail_sign_out
+        fail_sing_out
       end
     end
 
     def decode_jwt_payload
-      JWT.decode(request.headers['Authorization'].split.last,
-                 Rails.application.credentials.devise_jwt_secret_key).first
-    rescue JWT::DecodeError
+      auth_header = request.headers.fetch('Authorization', '').split.last
+      return nil if auth_header.blank?
+
+      JWT.decode(auth_header, Rails.application.credentials.devise_jwt_secret_key, true, algorithm: 'HS256').first
+    rescue JWT::DecodeError => e
+      Rails.logger.error("JWT decode error: #{e.message}")
       nil
     end
 
     def find_user_by_jwt_payload(jwt_payload)
       User.find_by(id: jwt_payload['sub'])
+    end
+
+    def generate_jwt_token(resource)
+      JWT.encode({ user_id: resource.id }, Rails.application.credentials.devise_jwt_secret_key, 'HS256')
     end
 
     def success_sign_out
